105863fc31c05eb1b92d64edc9cea1a4c1a0ccdf
董道祥 <dongdaoxiang@ncic.ac.cn>
2017-08-02 18:11:06 +0800

    add the zk test example

--- main/hello_server_main.go
+++ main/hello_server_main.go
@@ -4,14 +4,19 @@ import (
 	"flag"
 	"multi_connection_pool/server"
 	"runtime"
+	"strings"
 )
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	serverAddress := flag.String("server", "localhost:8888", "")
+	zkServers := flag.String("zk", "127.0.0.1:2181", "")
+	zkNode := flag.String("node", "/hello_server", "")
 	flag.Parse()
 	helloServer := server.HelloServer{
 		ServerAddress: *serverAddress,
+		ZkServers:     strings.Split(*zkServers, ","),
+		ZkNode:        *zkNode,
 	}
 	helloServer.Init()
 	helloServer.Serve()
--- server/hello_server.go
+++ server/hello_server.go
@@ -3,14 +3,18 @@ package server
 import (
 	"fmt"
 	"git.apache.org/thrift.git/lib/go/thrift"
+	"github.com/samuel/go-zookeeper/zk"
 	"multi_connection_pool/handler"
 	"multi_connection_pool/thrift/gen-go/hello"
+	"time"
 )
 
 type HelloServer struct {
 	ServerAddress string
 	server        *thrift.TSimpleServer
 	handler       *handler.HelloHandler
+	ZkServers     []string
+	ZkNode        string
 }
 
 func (p *HelloServer) Init() {
@@ -24,6 +28,50 @@ func (p *HelloServer) Init() {
 	p.handler = &handler.HelloHandler{}
 	processor := hello.NewRpcServiceProcessor(p.handler)
 	p.server = thrift.NewTSimpleServer4(processor, serverTransport, transportFactory, protocolFactory)
+
+	//create zk node
+
+	f := func(conn *zk.Conn, node string, flag int32) error {
+		_, err := conn.Create(node, nil, flag, zk.WorldACL(zk.PermAll))
+		if err != nil {
+			fmt.Printf("create parent node %s error :%s \n", node, err)
+		}
+		return err
+	}
+	go func() {
+		for {
+			conn, _, err := zk.Connect(p.ZkServers, time.Second*3)
+			if err != nil {
+				fmt.Errorf("connect to zk error")
+				time.Sleep(time.Second * 3)
+				conn.Close()
+				continue
+			}
+			node := fmt.Sprintf("%s/%s", p.ZkNode, p.ServerAddress)
+			if exist, _, _ := conn.Exists(p.ZkNode); !exist {
+				err := f(conn, p.ZkNode, int32(0))
+				if err != nil {
+					fmt.Printf("create parent node %s error :%s", p.ZkNode, err)
+					time.Sleep(time.Second * 3)
+					conn.Close()
+					continue
+				}
+
+			}
+			if exist, _, _ := conn.Exists(node); !exist {
+				fmt.Println("come here")
+				err := f(conn, node, int32(zk.FlagEphemeral))
+				if err != nil {
+					fmt.Printf("create child node:%s error :%s", node, err)
+					time.Sleep(time.Second * 3)
+					conn.Close()
+					continue
+				}
+			}
+			fmt.Println("create childNode success")
+			return
+		}
+	}()
 }
 func (p *HelloServer) Serve() error {
 	return p.server.Serve()
